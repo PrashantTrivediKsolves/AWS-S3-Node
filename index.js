5cb87bad2433cda4f959446044be0887d87bf4db
PrashantTrivediKsolves <prashant.kumar@ksolves.com>
2024-12-26 01:07:56 +0530

    Feature: added the function for put object into s3 - bucket

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("dotenv").config({ path: "./.env" });
 console.log("Does .env file exist?", fs.existsSync('./.env'));
 console.log("Loaded ENV Variables:", process.env);
 
-const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { S3Client, GetObjectCommand ,PutObjectCommand, ListObjectsV2Command, DeleteObjectCommand} = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
 
@@ -26,12 +26,44 @@ async function getObjectURL(key) {
   return url;
 }
 
+
+async function putObject(filename , contentType)
+{
+  const command = new PutObjectCommand({
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: `/uploads/user-uploads/${filename}`,
+    ContentType:contentType
+  })
+  const url = await getSignedUrl(s3Client ,command);
+  return url;
+
+}
+
+async function listObjects()
+{
+  const command =  new ListObjectsV2Command({
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: '/'
+  })
+  const result =  await s3Client.send(command);
+  console.log(result);
+}
 async function init() {
   try {
-    console.log("Url for panCard.jpg:", await getObjectURL("panCard.jpg"));
+    // console.log("Url for panCard.jpg:", await getObjectURL("/uploads/user-uploads/image-1735154328967.jpg"));
+    // console.log("URL for uploading", await putObject(`image-${Date.now()}.jpg`,"image/jpg"));
   } catch (error) {
     console.error("Error generating URL:", error);
   }
+  // await listObjects();
+
+  // this commad is used to delete the object
+
+  // const cmd = new DeleteObjectCommand({
+  //   Bucket :  process.env.AWS_BUCKET_NAME,
+  //   Key:'graphql.jpeg'
+  // })
+  // await s3Client.send(cmd);
 }
 
 init();
